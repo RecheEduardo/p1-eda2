e935e2d5c5c42b9719882030b771237a7e028571
Lucas Oliveira <lucas.oliveirah17@gmail.com>
2025-04-16 02:45:55 -0300

    Adicionado menu interativo e funções de criação de lista

--- aux_p1.c
+++ aux_p1.c
@@ -1,51 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-#include <conio.h>
+#include <time.h>
+#include <conio.h> // Biblioteca para ler as teclas direcionais do teclado sem a necessidade do ENTER
+#include <time.h>
 
-// PROCEDIMENTO DE EXIBIR AS OPCOES DO PROGRAMA NA TELA
-void exibeMenu_temp(){
+// MENU PRINCIPAL INTERATIVO
+int menuPrincipal(){
     char inputUser;
-    int row = 0, col = 0;
-    char selection[5][3];
-    setlocale(LC_ALL, "");
+    int row = 0, col = 0, algoritmo = 0;
+    char selection[4][3];
 
-    // Inicializacao da matriz
-    for(int j = 0; j < 5; j++){
+    // Zera matriz de seleção
+    for(int j = 0; j < 4; j++){
         for(int k = 0; k < 3; k++){
             selection[j][k] = ' ';
         }
-
     }
     selection[0][0] = '>';
 
     do{
         system("cls"); // Limpa a tela
-        printf("\n\t SELECIONE O ALGORITIMO QUE DESEJA REALIZAR O TESTE \n\n");
-        printf("\n%c BUBBLE SORT   \t%c INSERTION SORT \t%c SELECTION SORT", selection[0][0], selection[0][1], selection[0][2]);
-        printf("\n%c SHELL SORT    \t%c MERGE SORT     \t%c QUICK SORT", selection[1][0], selection[1][1], selection[1][2]);
-        printf("\n%c HEAP SORT     \t%c BUCKET SORT    \t%c RADIX SORT", selection[2][0], selection[2][1], selection[2][2]);
-        printf("\n%c COUNTING SORT \t%c TIM SORT", selection[3][0], selection[3][1]);
+        printf("\n######################## MENU #######################");
+        printf("\n%c BURBLE SORT   %c INSERTION SORT %c SELECTION SORT  ", selection[0][0], selection[0][1], selection[0][2]);
+        printf("\n%c SHELL SORT    %c MERGE SORT     %c QUICK SORT      ", selection[1][0], selection[1][1], selection[1][2]);
+        printf("\n%c HEAP SORT     %c BUCKET SORT    %c RADIX SORT (LSD)", selection[2][0], selection[2][1], selection[2][2]);
+        printf("\n%c COUNTING SORT %c TIM SORT       %c SAIR            ", selection[3][0], selection[3][1], selection[3][2]);
+        printf("\n#####################################################\n");
+
 
         // Ao clicar com as setas, envia 2 códigos, sendo o primeiro -32, e o segundo o código real
         inputUser = getch(); // Captura o primeiro código
         if(inputUser == -32){
             inputUser = getch(); // Captura o código real
+
             switch (inputUser) {
                 case 72: // Código para seta pra cima
                     selection[row][col] = ' ';
                     row--;
                     if(row < 0)
-                        row = 4;
+                        row = 3;
                     selection[row][col] = '>';
                     break;
+
                 case 80: // Código para seta pra baixo
                     selection[row][col] = ' ';
                     row++;
-                    if(row > 4)
+                    if(row > 3)
                         row = 0;
                     selection[row][col] = '>';
                     break;
+
                 case 75: // Código para seta pra esquerda
                     selection[row][col] = ' ';
                     col--;
@@ -53,6 +58,7 @@ void exibeMenu_temp(){
                         col = 2;
                     selection[row][col] = '>';
                     break;
+
                 case 77: // Código para seta pra direita
                     selection[row][col] = ' ';
                     col++;
@@ -62,65 +68,154 @@ void exibeMenu_temp(){
                     break;
             }
         }
-    }while(/*option != 4*/ 1);
-    printf("\nVocê selecionou a última opcao! Encerrando...\n");
-    system("PAUSE");
-    return 0;
+
+        else if(inputUser == 13){
+            break;
+        }
+
+        else {
+            printf("Comando Inválido!");
+        }
+    }while(1);
+    algoritmo = (((row + 1) * 3) + (col + 1)) - 3;
+    return algoritmo;
 }
 
-void exibeMenu(){
-        printf("\t========================================\n\n");
-        printf("\tOPCAO 1 -  BUBBLE SORT\n\n");
-        printf("\tOPCAO 2 -  INSERTION SORT\n\n");
-        printf("\tOPCAO 3 -  SELECTION SORT\n\n");
-        printf("\tOPCAO 4 -  SHELL SORT\n\n");
-        printf("\tOPCAO 5 -  MERGE SORT\n\n");
-        printf("\tOPCAO 6 -  QUICK SORT\n\n");
-        printf("\tOPCAO 7 -  HEAP SORT\n\n");
-        printf("\tOPCAO 8 -  BUCKET SORT\n\n");
-        printf("\tOPCAO 9 -  RADIX SORT\n\n");
-        printf("\tOPCAO 10 - COUNTING SORT\n\n");
-        printf("\tOPCAO 11 - TIM SORT\n\n");
-        printf("\tOPCAO 12 - SAIR\n\n");
-        printf("\t========================================\n\n");
+// SUBMENU PARA O TAMANHO DA LISTA
+unsigned long int menuTamanho(){
+
+
+    char inputUser;
+    int row = 0;
+    char selection[7];
+    unsigned int tamanho;
+
+    // Zera vetor de seleção
+    for(int i = 0; i < 7; i++) selection[i] = ' ';
+    selection[0] = '>';
+
+    do{
+        system("cls");
+        printf("\n###################### TAMANHO ######################");
+        printf("\n%c 10.000 ELEMENTOS", selection[0]);
+        printf("\n%c 50.000 ELEMENTOS", selection[1]);
+        printf("\n%c 100.000 ELEMENTOS", selection[2]);
+        printf("\n%c 200.000 ELEMENTOS", selection[3]);
+        printf("\n%c 500.000 ELEMENTOS", selection[4]);
+        printf("\n%c 1.000.000 ELEMENTOS", selection[5]);
+        printf("\n%c VOLTAR AO MENU PRINCIPAL", selection[6]);
+        printf("\n#####################################################\n");
+
+        // Ao clicar com as setas, envia 2 códigos, sendo o primeiro -32, e o segundo o código real
+        inputUser = getch(); // Captura o primeiro código
+        if(inputUser == -32){
+            inputUser = getch(); // Captura o código real
+
+            switch (inputUser) {
+                case 72: // Código para seta pra cima
+                    selection[row] = ' ';
+                    row--;
+                    if(row < 0)
+                        row = 6;
+                    selection[row] = '>';
+                    break;
+
+                case 80: // Código para seta pra baixo
+                    selection[row]= ' ';
+                    row++;
+                    if(row > 6)
+                        row = 0;
+                    selection[row] = '>';
+                    break;
+            }
+        }
+
+        else if(inputUser == 13){
+            break;
+        }
+    }while(1);
+
+    switch(row){
+        case 0:
+            tamanho = 10000;
+            return tamanho;
+
+        case 1:
+            tamanho = 50000;
+            return tamanho;
+
+        case 2:
+            tamanho = 100000;
+            return tamanho;
+
+        case 3:
+            tamanho = 200000;
+            return tamanho;
+
+        case 4:
+            tamanho = 500000;
+            return tamanho;
+
+        case 5:
+            tamanho = 1000000;
+            return tamanho;
+
+        case 6:
+            return 0;
+    }
+
+    return -1; // O retorno acontece pelo switch, porém coloquei um aqui para o compilador não ficar dando Warning
 }
 
-void selecionarTamanho(int opcaoTam){
-        printf("\n\t- SELECIONE O TAMANHO DO VETOR A SER TESTADO -\n\n");
-        printf("\t========================================\n\n");
-        printf("\t1 - 10.000    ELEMENTOS\n\n");
-        printf("\t2 - 50.000    ELEMENTOS\n\n");
-        printf("\t3 - 100.000   ELEMENTOS\n\n");
-        printf("\t4 - 200.000   ELEMENTOS\n\n");
-        printf("\t5 - 500.000   ELEMENTOS\n\n");
-        printf("\t6 - 1.000.000 ELEMENTOS\n\n");
-        printf("\t7 - VOLTAR AO MENU PRINCIPAL\n\n");
-        printf("\t========================================\n\n");
+unsigned long int *listaCrescente(unsigned long int tamanho){
+    unsigned long int *lista = malloc(tamanho * sizeof(unsigned int));
 
-        printf("\tEscolha uma opcao: ");
-        scanf("%d", &opcaoTam);
+    if (lista == NULL) {
+        printf("Erro na alocação de memória!\n");
+        exit(1);
+    }
 
-        switch (opcaoTam) {
-                case 1:
-                        printf("\n\tOpcao 1\n\n");
-                        break;
-                case 2:
-                        printf("\n\tOpcao 2\n\n");
-                        break;
-                case 3:
-                        printf("\n\tOpcao 3\n\n");
-                        break;
-                case 4:
-                        printf("\n\tOpcao 4\n\n");
-                        break;
-                case 5:
-                        printf("\n\tOpcao 5\n\n");
-                        break;
-                case 6:
-                        printf("\n\tOpcao 6\n\n");
-                        break;
-                case 7:
-                        printf("\n\tOpcao 7\n\n");
-                        break;
-        } while (opcaoTam != 12);
+    // Gera um número aleatório para cada item da lista
+    for(unsigned long int i = 0; i < tamanho; i++){
+        lista[i] = i;
+    }
+    return lista;
 }
+
+unsigned long int *listaDecrescente(unsigned long int tamanho){
+    unsigned long int *lista = malloc(tamanho * sizeof(unsigned int));
+
+    if (lista == NULL) {
+        printf("Erro na alocação de memória!\n");
+        exit(1);
+    }
+
+    // Gera um número aleatório para cada item da lista
+    for(unsigned long int i = 0; i < tamanho; i++){
+        lista[i] = tamanho - i;
+    }
+    return lista;
+}
+
+unsigned long int *listaAleatoria(unsigned long int tamanho){
+    unsigned long int *lista;
+    unsigned long int tmp, j;
+
+    srand((unsigned) time(NULL));
+
+    // Cria primeiro uma lista crescente
+    lista = listaCrescente(tamanho);
+
+    // E agora embaralha a lista
+    for(unsigned long int i = 0; i < tamanho; i++){
+        j = rand() % (i + 1);
+        tmp = lista[j];
+        lista[j] = lista[i];
+        lista[i] = tmp;
+    }
+
+    return lista;
+}
+//  nomeDoAlgoritmoTeste(unsigned long int *lista, unsigned long int tamanho){
+//      CÓDIGO...
+//  }
